5780cc8afda7112cae32ade0fc40ed65f3f6d63c
amazingHuo <15710021168@163.com>
2024-07-20 11:03:59 +0800

    add

--- src/index.js
+++ src/index.js
@@ -4,8 +4,9 @@ import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import { Provider } from "react-redux";
-import store, { persistor } from "./store";
-import { PersistGate } from "redux-persist/lib/integration/react";
+import store from "./store";
+// { persistor }
+// import { PersistGate } from "redux-persist/lib/integration/react";
 
 import "./asserts/css/tailwind.css";
 import "./asserts/css/init.css";
@@ -15,9 +16,9 @@ const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <Provider store={store}>
-      <PersistGate loading={null} persistor={persistor}>
+      {/* <PersistGate loading={null} persistor={persistor}> */}
         <App />
-      </PersistGate>
+      {/* </PersistGate> */}
     </Provider>
   </React.StrictMode>
 );
--- src/store/index.js
+++ src/store/index.js
@@ -3,14 +3,17 @@ import { persistStore, persistReducer } from "redux-persist";
 import storage from "redux-persist/lib/storage";
 import reducer from "./reducer";
 
-const persistConfig = {
-  key: "root",
-  storage: storage,
-};
-const myPersistReducer = persistReducer(persistConfig, reducer);
+// const persistConfig = {
+//   key: "root",
+//   storage: storage,
+// };
+// const myPersistReducer = persistReducer(persistConfig, reducer);
 
-const store = createStore(myPersistReducer);
+// const store = createStore(myPersistReducer);
 
-export const persistor = persistStore(store);
+// export const persistor = persistStore(store);
+
+
+const store = createStore(reducer)
 
 export default store;
