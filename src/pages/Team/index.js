a3a878d4db5de94de7c806c91b659af06762a325
amazingHuo <15710021168@163.com>
2024-07-20 10:09:11 +0800

    add

--- src/pages/Team/index.js
+++ src/pages/Team/index.js
@@ -9,11 +9,18 @@ const Team = () => {
   const address = useSelector((state) => state.address);
 
   const [data, setData] = useState("");
+  const [page, setPage] = useState("1");
+  const [total, setTotal] = useState("");
   const getInfo = () => {
     http
-      .get("/Aleo/UserCenter")
+      .get("/Aleo/MyTeam", {
+        params: {
+          page,
+        },
+      })
       .then((res) => {
-        setData(res.data.data.recommend);
+        setData(res.data.data.list);
+        setTotal(res.data.data.totalPage)
       })
       .catch((err) => {
         console.log(err);
@@ -31,22 +38,22 @@ const Team = () => {
     <div className="contentHome text-white">
       <div className="text-center my-5 text-base">{t("team.myTeam")}</div>
       <div className="border mx-5 p-5 text-center">
-        <div className="flex items-center justify-between text-sm mb-7">
-          <span className="text-center">{t("team.time")}</span>
-          {/* <span className="w-1/4">{t("team.number")}</span>
-          <span className="w-1/4">{t("team.TeamPerformance")}</span> */}
-          <span className="text-center">{t("team.address")}</span>
+        <div className="flex items-center text-sm mb-7 text-left">
+          <span className="w-1/4 ">{t("team.time")}</span>
+          <span className="w-1/4 text-center">{t("team.number")}</span>
+          <span className="w-1/4 ">{t("team.TeamPerformance")}</span>
+          <span className="w-1/4 text-right">{t("team.address")}</span>
         </div>
         {data.length > 0 ? (
           data.map((list, index) => {
             return (
               <div
-                className="flex items-center justify-between text-sm text mt-2"
+                className="flex items-center justify-between text-xs text mt-4"
                 key={index}
               >
-                <span className="text-left">{list.date}</span>
-                {/* <span className="w-1/4">1</span>
-                <span className="w-1/4">2</span> */}
+                <span className="text-left">{list.date.split(' ')[0]}</span>
+                <span className="">{list.recommended_quantity}</span>
+                <span className="">{list.team_performance}</span>
                 <span className="text-right">{list.address}</span>
               </div>
             );
