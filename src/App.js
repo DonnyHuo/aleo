2604b3b076b45ec1bb5be3f5bba81a834687f219
amazingHuo <15710021168@163.com>
2024-07-21 17:05:59 +0800

    add

--- src/App.js
+++ src/App.js
@@ -13,25 +13,24 @@ import Team from "./pages/Team";
 import Pledge from "./pages/Pledge";
 import Computing from "./pages/Computing";
 
-
-
 function App() {
-
   return (
     <BrowserRouter>
       <div className="App">
         <Header />
-        <Routes>
-          <Route path="/" element={<Home />}></Route>
-          <Route path="/mine" element={<Mine />}></Route>
-          <Route path="/raise" element={<Raise />}></Route>
-          <Route path="/about" element={<About />}></Route>
-          <Route path="/black" element={<Black />}></Route>
-          <Route path="/invite" element={<Invite />}></Route>
-          <Route path="/team" element={<Team />}></Route>
-          <Route path="/pledge" element={<Pledge />}></Route>
-          <Route path="/computing" element={<Computing />}></Route>
-        </Routes>
+        <div className="appContent">
+          <Routes>
+            <Route path="/" element={<Home />}></Route>
+            <Route path="/mine" element={<Mine />}></Route>
+            <Route path="/raise" element={<Raise />}></Route>
+            <Route path="/about" element={<About />}></Route>
+            <Route path="/black" element={<Black />}></Route>
+            <Route path="/invite" element={<Invite />}></Route>
+            <Route path="/team" element={<Team />}></Route>
+            <Route path="/pledge" element={<Pledge />}></Route>
+            <Route path="/computing" element={<Computing />}></Route>
+          </Routes>
+        </div>
       </div>
     </BrowserRouter>
   );
